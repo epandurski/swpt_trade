aa89acee2c2d33d5cf30712a205a9d3e0d4f77c7
Evgeni Pandurski <epandurski@gmail.com>
2026-01-16 02:24:33 +0200

    Move roll_delayed_account_transfers CLI command

--- swpt_trade/cli/db_tasks_processing.py
+++ swpt_trade/cli/db_tasks_processing.py
@@ -53,6 +53,56 @@ def handle_pristine_collectors(threads, wait, quit_early):
     sync_collectors.handle_pristine_collectors(threads, wait, quit_early)
 
 
+@swpt_trade.command("roll_delayed_account_transfers")
+@with_appcontext
+@click.option(
+    "-w",
+    "--wait",
+    type=float,
+    help=(
+        "Poll the worker's database every FLOAT seconds for delayed"
+        " account transfers ready to be processed. If not specified,"
+        " 1/12th of the value of the TRANSFERS_HEALTHY_MAX_COMMIT_DELAY"
+        " environment variable will be used, defaulting to 10 minutes if it"
+        " is empty."
+    ),
+)
+@click.option(
+    "--quit-early",
+    is_flag=True,
+    default=False,
+    help="Exit after some time (mainly useful during testing).",
+)
+def roll_delayed_account_transfers(wait, quit_early):
+    """Run a process which polls the worker's database for delayed
+    account transfers ready to be processed.
+    """
+    from swpt_trade.process_transfers import process_delayed_account_transfers
+
+    cfg = current_app.config
+    wait_interval: timedelta = cfg["TRANSFERS_HEALTHY_MAX_COMMIT_DELAY"] / 12
+    wait_seconds = (
+        wait
+        if wait is not None
+        else wait_interval.total_seconds()
+    )
+    logger = logging.getLogger(__name__)
+    logger.info("Started rolling delayed account transfers.")
+
+    while True:
+        logger.info(
+            "Looking for delayed account transfers ready to be processed."
+        )
+        started_at = time.time()
+        n = process_delayed_account_transfers()
+        logger.info("Replayed %d account transfer messages.", n)
+
+        if quit_early:
+            break
+        if wait_seconds > 0.0:  # pragma: no cover
+            time.sleep(max(0.0, wait_seconds + started_at - time.time()))
+
+
 @swpt_trade.command("trigger_transfers")
 @with_appcontext
 @click.option(
